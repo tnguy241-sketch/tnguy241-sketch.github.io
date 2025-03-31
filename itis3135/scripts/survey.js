6fcc2021276cc599e4d003669bc18b378f7bf9be
tnguy241-sketch <tnguy241@charlotte.edu>
2025-03-31 14:52:11 -0400

    file update

--- itis3135/scripts/survey.js
+++ itis3135/scripts/survey.js
@@ -11,31 +11,31 @@ function validateForm() {
     const addCourseButton=document.getElementById("addCourseButton");
     const deleteCourseButton=document.getElementById("deleteCourseButton");
     const agree=document.getElementById("agree");
-    if(name==""){
+    if (name=""){
         alert("Enter your name");
         return false;
     }
-    if(mascot=""){
+    if (mascot=""){
         alert("Enter your mascot");
         return false;
     }
-    if(imageCaption=""){
+    if (imageCaption=""){
         alert("Enter a caption");
         return false;
     }
-    if(personal=""){
+    if (personal=""){
         alert("Enter your personal background");
         return false;
     }
-    if(profession=""){
+    if (profession=""){
         alert("Enter your professional background");
         return false;
     }
-    if(academic=""){
+    if (academic=""){
         alert("Enter your academic background");
         return false;
     }
-    if(web=""){
+    if (web=""){
         alert("Enter your background in web development");
         return false;
     }
@@ -43,25 +43,25 @@ function validateForm() {
         alert("Enter your primary computer platform");
         return false;
     }
-    addCourseButton.addEventListener('click',()=>{
+    addCourseButton.addEventListener('click',() => {
         const newCourse=document.createElement('div');
         newCourse.classList.add('course');
         newCourse.textContent="New Course";
         courses.appendChild(newCourse);
     });
-    deleteCourseButton.addEventListener('click',()=>{
+    deleteCourseButton.addEventListener('click',() => {
         const lastCourse=courses.lastElementChild;
         if(lastCourse){
             lastCourse.remove();
         }
     })
     agree.addEventListener('change',function(){
-        if(agree.checked){
-            console.log("Checkbox is checked.")
+        if (agree.checked){
+            console.log("Checkbox is checked.");
             return true;
         }
-        else{
-            console.log("Please click the checkbox.")
+        else {
+            console.log("Please click the checkbox.");
             return false;
         }
     })
